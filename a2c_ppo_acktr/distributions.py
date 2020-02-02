75f60d34a85fcb5ea0558707888c78cd9cc9d18b
Rui Liu <ruiliu310@gmail.com>
2020-02-01 20:34:56 -0500

    fix bug when computing log_probs

--- a2c_ppo_acktr/distributions.py
+++ a2c_ppo_acktr/distributions.py
@@ -47,7 +47,7 @@ class FixedNormal(torch.distributions.Normal):
 # Bernoulli
 class FixedBernoulli(torch.distributions.Bernoulli):
     def log_probs(self, actions):
-        return super.log_prob(actions).view(actions.size(0), -1).sum(-1).unsqueeze(-1)
+        return super().log_prob(actions).view(actions.size(0), -1).sum(-1).unsqueeze(-1)
 
     def entropy(self):
         return super().entropy().sum(-1)
